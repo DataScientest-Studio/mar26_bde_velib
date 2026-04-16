d1e3319095c48fda3dec64518d3ba8e826b77e96
b-tilmatine <b.tilmatine@gmail.com>
2026-04-16 11:12:35 +0200

    Ajout des dépendances du projet et des scripts

---
+++ src/data/import.py
@@ -0,0 +1,57 @@
+import os
+import requests
+import pymongo
+from datetime import datetime
+from datetime import datetime, timezone
+
+import os
+from dotenv import load_dotenv
+
+#API_URL = "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json"
+
+load_dotenv()
+MONGO_URI = os.getenv("MONGO_URL")
+
+#MONGO_URI = os.getenv("MONGO_URI", "mongodb://admin:password1234@localhost:27017/")
+
+def appel_url(url: str , headers_dict = None ) -> dict:
+    
+    headers = headers_dict if headers_dict else {}
+
+    response = requests.get(url, headers=headers,  timeout=10)
+    response.raise_for_status()  
+    return response.json()
+
+
+
+def insert_mongo(client: pymongo.MongoClient, data: dict, table_name: str) -> None:
+    col = client["velib"][table_name]
+    result = col.insert_one(data)
+    #result = col.insert_many(data)
+    print(f"Inserted: {result.inserted_id}")
+
+def main(base , API_URL , headers_dict = None  ):
+    client = pymongo.MongoClient(MONGO_URI)  
+    try:
+        data = appel_url(API_URL, headers_dict)
+        data["_ingested_at"] = datetime.now(timezone.utc)
+        insert_mongo(client, data, base )
+
+    except Exception as e:
+        print(f"Erreur : {e}")
+    finally:
+        client.close()
+
+if __name__ == "__main__":
+    main( "station_status" ,"https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json" )
+    main( "station_info" ,"https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json" )
+
+    #main( "medeo" ,f"https://api.openweathermap.org/data/2.5/weather?lat=48.8566&lon=2.3522&appid={os.getenv("METEO_KEY")}" )
+
+    main( "medeo" ,f"https://api.openweathermap.org/data/2.5/weather?units=metric&lang=fr&q=paris&appid={os.getenv("METEO_KEY")}" )
+
+    headers_dict ={
+        "apikey": os.getenv("RER_KEY") ,
+
+    }
+    main( "RER" ,"https://prim.iledefrance-mobilites.fr/marketplace//estimated-timetable?LineRef=ALL" , headers_dict )
---
+++ src/data/transform.py
@@ -0,0 +1,284 @@
+
+import pymongo
+import pandas as pd
+import psycopg2
+import psycopg2.extras
+from datetime import datetime, timezone
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+MONGO_URL = os.getenv("MONGO_URL")
+
+
+
+def message(pg_conn):
+
+    """
+    recuper le dernier heure du dernier message dans la table station_status_flat
+    :
+
+    sortie : date 
+    
+    """
+    with pg_conn.cursor() as cur:
+        cur.execute("SELECT inserted_at FROM station_status_flat ORDER BY inserted_at DESC LIMIT 1;")
+        row = cur.fetchone()
+        if row:
+         
+            return row[0] 
+        return datetime(2000, 1, 1, tzinfo=timezone.utc)
+
+
+
+def mango_station_satut( pg_conn): 
+
+
+    """
+    Extration des donnée depuis la base de données MongoDB .
+    
+    
+    """
+    myclient = pymongo.MongoClient(MONGO_URL)
+    try :
+        mydb = myclient["velib"]
+        mycol = mydb["station_status"]
+
+
+        date = message(pg_conn)
+        print(f"Dernier enregistrement :  {date}")
+        stations_filtre = mycol.find({"_ingested_at": {"$gt": date }})
+        
+        table_stations = []
+
+        for stations in stations_filtre :
+            
+            extracted_at = stations['_ingested_at']
+            print(f" date : {extracted_at}")
+
+
+        
+            stations = stations['data']['stations']
+            #table_stations = []
+            print('test')
+            for station in  stations :
+                
+                
+
+                velo_total  = station.get('num_bikes_available')
+                
+                docker_total = station.get('num_docks_available')
+                station_id = station['station_id']
+                #print(f" Station Id { station_id}  ")
+
+                mechanical = 0
+                ebike = 0
+
+                for disponible in  station.get("num_bikes_available_types"  , [])  :
+
+                    mechanical += disponible.get("mechanical" , 0) 
+                    ebike += disponible.get("ebike" ,0)
+
+  
+
+                table_stations.append({
+                    "station_id": station_id,
+                    "docker_total" : docker_total ,
+                    "mechanical" : mechanical ,
+                    #"velo_total": velo_total,
+                    "ebike" : ebike, 
+                    "date" : extracted_at
+
+
+                    #"extracted_at": extracted_at,
+
+                })
+
+                
+                #return extracted_at , table_stations
+        return table_stations
+            
+    except Exception as e:
+        print(f"Erreur : {e}")
+    finally:
+        myclient.close()
+
+
+def mango_station_info( pg_conn): 
+
+
+    """
+    Extration des donnée depuis la base de données MongoDB .
+    
+    
+    """
+    myclient = pymongo.MongoClient(MONGO_URL)
+    try :
+        mydb = myclient["velib"]
+        mycol = mydb["station_info"]
+
+
+        date = message(pg_conn)
+        print(f"Dernier enregistrement :   {date}")
+        stations_filtre = mycol.find({"_ingested_at": {"$gt": date }})
+        
+        table_stations = []
+
+        for stations in stations_filtre :
+            
+            extracted_at = stations['_ingested_at']
+            print(f" date {extracted_at}")
+
+
+        
+            stations = stations['data']['stations']
+            #table_stations = []
+            for station in  stations :
+                
+
+
+                station_id = station['station_id']
+                name = station['name']
+                lat = station['lat']
+                lon = station['lon']
+  
+                table_stations.append({
+                    "station_id": station_id,
+                    "name" : name ,
+                    "lat" : lat ,
+                    "lon" : lon
+                })
+    
+            
+        return table_stations
+    except Exception as e:
+        print(f"Erreur : {e}")
+    finally:
+        myclient.close()
+
+
+def create_table(pg_conn) -> None:
+
+    """
+    Creation de la table station_status_flat dans PostgreSQL
+    
+    """
+    with pg_conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS station_status_flat (
+                id                  SERIAL PRIMARY KEY,
+                station_id          BIGINT NOT NULL,
+                num_docks_available INT,
+                num_bikes_mechanical INT,
+                num_bikes_ebike     INT,
+                inserted_at         TIMESTAMPTZ DEFAULT NOW()
+            );
+                    
+            --ALTER TABLE station_info_flat ALTER COLUMN station_id TYPE BIGINT;
+            --ALTER TABLE station_status_flat ALTER COLUMN station_id TYPE BIGINT;
+                    
+
+            CREATE TABLE IF NOT EXISTS station_info_flat ( 
+                station_id    BIGINT PRIMARY KEY, 
+                name          TEXT NOT NULL,
+                lat           DOUBLE PRECISION NOT NULL,
+                lon           DOUBLE PRECISION NOT NULL) ;
+        """)
+    pg_conn.commit()
+
+
+
+def insert_stations(pg_conn, stations: list[dict]) -> None:
+
+    """ 
+    Insertion des données de station_status dans la table
+    """
+    if not stations:
+        print("Aucune station à insérer.")
+        return
+
+    with pg_conn.cursor() as cur:
+        psycopg2.extras.execute_values(
+            cur,
+            """
+            INSERT INTO station_status_flat
+                (station_id, num_docks_available, num_bikes_mechanical, num_bikes_ebike, inserted_at)
+            VALUES %s
+            """,
+            [(s["station_id"], s["docker_total"], s["mechanical"], s["ebike"] , s["date"])
+             for s in stations]
+        )
+    pg_conn.commit()
+    print(f"{len(stations)} Statut stations insérées dans PostgreSQL.")
+  
+
+
+def insert_stations_info(pg_conn, stations: list[dict]) -> None:
+
+    """ 
+    Insertion ou mise à jour des informations de référence des stations
+    """
+    if not stations:
+        print("Aucune station à insérer.")
+        return
+
+    with pg_conn.cursor() as cur:
+        psycopg2.extras.execute_values(
+            cur,
+            """
+            INSERT INTO station_info_flat (station_id, name, lat, lon)
+            VALUES %s
+            ON CONFLICT (station_id) 
+            DO UPDATE SET 
+                name = EXCLUDED.name,
+                lat = EXCLUDED.lat,
+                lon = EXCLUDED.lon;
+            """,
+            [(s["station_id"], s["name"], s["lat"], s["lon"] )
+             for s in stations]
+        )
+    pg_conn.commit()
+    print(f"{len(stations)} information  stations insérées dans PostgreSQL.")
+
+
+    
+
+def main() : 
+
+
+    
+    pg_conn = psycopg2.connect(
+            database="velib",
+            user= os.getenv("PG_Login"),
+            password= os.getenv("PG_password"),
+            host= os.getenv("PG_host"),
+            port= os.getenv("PG_port")
+        )
+    
+    try:
+        create_table(pg_conn) 
+
+
+        station =  mango_station_info(pg_conn)
+        insert_stations_info(pg_conn, station)
+
+        
+        station = mango_station_satut(pg_conn)
+        insert_stations(pg_conn, station)
+
+    except Exception as e:
+        pg_conn.rollback()
+        print(f"Erreur : {e}")
+    finally:
+
+        pg_conn.close()
+
+
+
+   
+
+
+
+
+main()
