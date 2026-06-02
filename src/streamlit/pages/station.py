dafc07b769e1264d2c2295091f0b37d589b9c2cc
b-tilmatine <b.tilmatine@gmail.com>
2026-06-03 00:43:28 +0200

    Fix hour formatting in API request and enhance predictions data handling

--- src/streamlit/pages/station.py
+++ src/streamlit/pages/station.py
@@ -31,13 +31,13 @@ st.title(f"Station #{id_station}")
 try:
     with st.spinner("Chargement des données..."):
         x = datetime.datetime.now()
-        hours = "".join([f"&heure={i}:00" for i in range(x.hour, 24)])
+        hours = "".join([f"&heure={i:02d}:00" for i in range(x.hour, 24)])
 
         r_station = requests.get(f"http://{HOST}:8000/v1/stations/{id_station}", headers=headers)
         r_etat    = requests.get(f"http://{HOST}:8000/v1/stations/{id_station}/etat", headers=headers)
         r_semaine = requests.get(f"http://{HOST}:8000/v1/statistiques/station/{id_station}/semaine", headers=headers)
         r_pred    = requests.get(f"http://{HOST}:8000/v1/predictions/station?id_station={id_station}{hours}", headers=headers)
-
+        #print(f"http://{HOST}:8000/v1/predictions/station?id_station={id_station}{hours}")
     # ══════════════════════════════════════════════
     # PHASE 2 : Affichage après réception
     # ══════════════════════════════════════════════
@@ -70,7 +70,11 @@ try:
 
     # ── Tendance 7 jours ───────────────────────────
     if r_semaine.status_code == 200:
+        
         df_sem = pd.DataFrame(r_semaine.json())
+        df_sem = df_sem.fillna(0)
+        print(f" df_sem : {df_sem}")
+
         st.subheader("Tendance sur 7 jours")
         st_apexcharts(
             {
@@ -92,14 +96,21 @@ try:
     print(f"r_pred.status_code {r_pred.status_code} {r_pred.status_code == 200:}")
     if r_pred.status_code == 200:
         data_pred = r_pred.json()
-    
+        print("")
         # Une seule prédiction (dict) ou plusieurs (liste)
+        data_pred = r_pred.json()
+
+        # Extraire la liste de prédictions imbriquée
         if isinstance(data_pred, list):
-            df_pred = pd.DataFrame(data_pred)
+            predictions_list = data_pred[0]["predictions"]
+        elif "predictions" in data_pred:
+            predictions_list = data_pred["predictions"]
         else:
-            df_pred = pd.DataFrame([data_pred])
-        
+            predictions_list = [data_pred]
+
+        df_pred = pd.DataFrame(predictions_list)
         print(f"df_pred : {df_pred}")
+
         
         st.subheader("Prédictions du jour")
         st_apexcharts(
